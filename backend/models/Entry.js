5a090c7059fd9d0b6307b877b23a20d7ae6e8c1d
omar-gaal <omargaal123@gmail.com>
2025-07-09 21:36:51 +0200

    Schema for entry

---
+++ backend/models/Entry.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const EntrySchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Entry", EntrySchema);
