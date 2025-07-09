12410cd63bee96da7e209085a262b7a510f7dee1
omar-gaal <omargaal123@gmail.com>
2025-07-09 21:35:48 +0200

    connecting to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,32 @@
-const express = require("express");
 const cors = require("cors");
-const app = express();
 
-app.use(cors());
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
 app.use(express.json());
 
-app.get("/", (reg, res) => {
-  res.send("Hello from backend 👋");
+app.use(cors());
+
+const entryRoutes = require("./routes/entryRoutes");
+app.use("/entry", entryRoutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((error) => console.log("❌ MongoDB connection error:", error));
+
+// Teest route
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+  ƒ;
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on http:localhost:{PORT}`);
+app.listen(5000, () => {
+  console.log("🚀 Server running on http://localhost:5000");
 });
