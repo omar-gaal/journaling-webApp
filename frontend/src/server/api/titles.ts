51c0bb2c10c1d03de869b0ba14199af9934dc631
omar-gaal <omargaal123@gmail.com>
2025-08-23 17:43:24 +0200

    added id to endpoint

--- frontend/src/server/api/titles.ts
+++ frontend/src/server/api/titles.ts
@@ -1,12 +1,18 @@
 import type { Entry } from "~/types/entry";
+import { Title } from '../../../.nuxt/components';
 
 
 export default defineEventHandler(async (event) => {
     
     try {
         const entries = await $fetch<Entry[]>("http://localhost:5001/entry");
-        const titles = entries.map(entries => entries.title);
-        return titles;
+        
+        const titlesWithIds = entries.map(entry => ({
+            _id: entry._id,
+            title: entry.title
+        }));
+        return titlesWithIds;
+        
     } catch (error) {
         console.log("Failed to fetch titles:", error)
         return { error: "Failed to fetch titles"};
