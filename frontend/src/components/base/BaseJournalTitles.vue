8d3f607a50faa7abd8bba04ec0a0dc937a61f92c
omar-gaal <omargaal123@gmail.com>
2025-08-23 19:47:27 +0200

    made new component to clean up sidebar

---
+++ frontend/src/components/base/BaseJournalTitles.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+    titles: Array<{ _id: string; title: string;}> | null | undefined;
+    pending: boolean
+}>();
+</script>
+
+<template>
+    <div class="jounal-titles">
+        <h3 v-if="props.pending">Loading Titles....</h3>
+        <template v-else-if="props.titles && props.titles.length > 0">
+            <h3 v-for="entry in props.titles" :key="entry._id">
+                <NuxtLink :to="`/entries/${entry._id}`" class="link-style">
+                    - {{ entry.title }}
+                </NuxtLink>
+            </h3>
+        </template>
+        <h3 v-else>No titles found.</h3>
+    </div>
+</template>
+
+
+<style scoped>
+.link-style {
+  color: inherit; 
+  text-decoration: none; 
+}
+
+
+.link-style:hover {
+  text-decoration: underline;
+  color: #555; 
+}
+</style>
