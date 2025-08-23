51f03c43b98c651d2318d260ab06979f33d680db
omar-gaal <omargaal123@gmail.com>
2025-08-23 19:48:56 +0200

    added sidebar to all pages

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,14 +1,22 @@
 
-<script lang="ts">
+<script setup lang="ts">
+import { PartialSidebar } from '#components';
 
+const isOpen = useSidebar();
 
 </script>
 
 <template>
     <div>
         <PartialMainHeader/>
+         
+        <Transition>
+           <PartialSidebar v-if="isOpen"/>
+        </Transition>
+        
         <div>
             <slot/>
         </div>
+
     </div>
 </template>
