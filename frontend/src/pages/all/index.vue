341b825139404a0f17fac58c7004d2ee5fbe2c33
omar-gaal <omargaal123@gmail.com>
2025-09-02 13:58:17 +0200

    new page for list of title with link to every content pages/all

--- frontend/src/pages/all/index.vue
+++ frontend/src/pages/all/index.vue
@@ -1,6 +1,40 @@
 <script setup lang="ts">
+
+const {data: titles, pending } = await useFetch<{ _id: string, title: string}[]>('/api/titles')
+
 </script>
 
 <template>
-    <h1>All entries </h1>
+
+    <div class="all-entry-titles">
+        <h1>All entries </h1>
+        <div class="title-list">
+            <BaseJournalTitles    
+                class="journalTitles"
+                :titles="titles" 
+                :pending="pending"
+                />
+
+        </div>
+    </div>
+
+
 </template>
+
+<style>
+
+.all-entry-titles {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+    margin-top: 24px;
+}
+.title-list {
+    display: flex;
+    flex-direction: column;
+    gap: 6rem;
+}
+
+</style>
